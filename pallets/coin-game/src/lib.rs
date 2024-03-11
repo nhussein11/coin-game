19f9465fe984900b71fbd0fa00ec87e8ac413919
nhussein11 <nicolashussein14@gmail.com>
2024-03-11 16:36:23 -0300

    fix: integrating random pallet to incorporate randomness

--- pallets/coin-game/src/lib.rs
+++ pallets/coin-game/src/lib.rs
@@ -22,6 +22,9 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
+	use frame_support::PalletId;
+	use frame_support::traits::Randomness;
+
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
 
@@ -35,6 +38,11 @@ pub mod pallet {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 		/// Type representing the weight of this pallet
 		type WeightInfo: WeightInfo;
+		/// Type representing the Pallet ID
+		#[pallet::constant]
+		type PalletId: Get<PalletId>;
+		/// Type representing the random number generator
+		type MyRandomness: Randomness<Self::Hash, BlockNumberFor<Self>>;
 	}
 
 	#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, MaxEncodedLen, TypeInfo, PartialOrd, Default)]
@@ -57,11 +65,13 @@ pub mod pallet {
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		/// A new coin has been created
-		CoinCreated(T::AccountId, Coin),
+		CoinCreated(T::AccountId),
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
+		/// Coin already exists
+		CoinAlreadyExists,
 	}
 
 	#[pallet::call]
@@ -69,14 +79,47 @@ pub mod pallet {
 		#[pallet::call_index(0)]
 		#[pallet::weight(T::WeightInfo::default_weight())]
 		pub fn create_coin(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+			let who = ensure_signed(origin)?;
+			let _  = Self::do_create_coin(&who);
+			Self::deposit_event(Event::CoinCreated(who));
+			Ok(())
+		}
+	}
+
+	impl<T: Config> Pallet<T> {
+		pub fn do_create_coin(who: &T::AccountId) -> DispatchResult {
 			let coin = Coin {
-				side: CoinSide::Head,
+				side: Self::random_coin_side(),
 			};
-			CoinStorage::<T>::insert(&_who, coin.clone());
-			Self::deposit_event(Event::CoinCreated(_who, coin));
+
+			if CoinStorage::<T>::contains_key(who) {
+				return Err(Error::<T>::CoinAlreadyExists.into());
+			}
+
+			CoinStorage::<T>::insert(who, coin);
+
 			Ok(())
 		}
 
+		pub fn random_coin_side() -> CoinSide {
+			let block_number = <frame_system::Pallet<T>>::block_number();
+			let seed = block_number.try_into().unwrap_or_else(|_| 0u32);
+
+			if Self::generate_insecure_random_boolean(seed) {
+				CoinSide::Head
+			} else {
+				CoinSide::Tail
+			}
+		}
+
+
+		// TODO: check for safer alternatives to generate random numbers in this env
+		// You should call this function with different seed values, in this case I'm are using the block number as seed
+		pub fn generate_insecure_random_boolean(seed: u32) -> bool {
+			let (random_seed, _) = T::MyRandomness::random(&(T::PalletId::get(), seed).encode());
+			let random_number = <u32>::decode(&mut random_seed.as_ref())
+				.expect("secure hashes should always be bigger than u32; qed");
+			random_number % 2 == 0
+		}
 	}
 }
