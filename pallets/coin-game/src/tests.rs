c9d13659ef9d5b93563c78ecfff7340b7f35a4aa
nhussein11 <nicolashussein14@gmail.com>
2024-03-14 16:56:42 -0300

    fix: adding error handling testing to unit tests

--- pallets/coin-game/src/tests.rs
+++ pallets/coin-game/src/tests.rs
@@ -1,7 +1,7 @@
 use frame_system::Origin;
-use crate::{mock::*, Event};
+use crate::{mock::*, Event, Error};
 use frame_support::{assert_noop, assert_ok};
-// use sp_runtime::AccountId32;
+use sp_runtime::DispatchError;
 
 // pub const ALICE: AccountId32 = AccountId32::new([1u8; 32]);
 
@@ -11,6 +11,19 @@ fn create_coin_works() {
 		assert_ok!(CoinGame::create_coin(RuntimeOrigin::signed(1u64)));
 		let expected_event = RuntimeEvent::CoinGame(Event::CoinCreated(1));
 		assert_eq!(last_event(), expected_event);
+	});
+}
+
+
+#[test]
+fn create_coin_fails() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(CoinGame::create_coin(RuntimeOrigin::signed(1u64)));
+		assert_eq!(
+			CoinGame::create_coin(RuntimeOrigin::signed(1u64)),
+			Err(DispatchError::from(Error::<Test>::CoinAlreadyExists))
+		);
+
 	});
 }
 
